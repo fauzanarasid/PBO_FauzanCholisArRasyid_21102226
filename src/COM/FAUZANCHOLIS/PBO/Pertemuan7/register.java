feec4190a0a0854d3899e3f7b1788fab4ea35214
fauzanarasid <fauzanarasid@gmail.com>
2023-05-31 15:49:26 +0700

    first commit

---
+++ src/COM/FAUZANCHOLIS/PBO/Pertemuan7/register.java
@@ -0,0 +1,15 @@
+package COM.FAUZANCHOLIS.PBO.Pertemuan7;
+
+import javax.swing.*;
+
+public class register {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JComboBox comboBox1;
+    private JTextField textField3;
+    private JRadioButton lakiLakiRadioButton;
+    private JRadioButton perempuanRadioButton;
+    private JTextArea textArea1;
+    private JButton saveButton;
+    private JButton clearButton;
+}
